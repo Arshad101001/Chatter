7f04cf7114bf938cd8b1b999839286cdaf65dd9c
Arshad101001 <arshadansari101001@gmail.com>
2025-11-26 19:13:36 +0530

    sidebar completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,9 +14,9 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 // req.body middleware
-app.use(express.json());  // handling json data
-app.use(express.urlencoded({ extended: false })); // handling form data
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
+app.use(express.json({ limit: "10mb" }));  // handling json data && adding limit so our backend can handle large file or request
+app.use(express.urlencoded({ extended: true, limit: "10mb" })); // handling form data && adding limit so our backend can handle large file or request
+app.use(cors({ origin: ENV.CLIENT_URL, methods: "GET, POST, PUT, DELETE", credentials: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes)
