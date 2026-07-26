212757f3f68a3607b5120809ea04873caed70d11
Arshad Ansari <arshadansari101001@gmail.com>
2026-07-27 01:08:51 +0530

    feat: backend sockets implemented for group

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -3,6 +3,8 @@ import http from 'http';
 import express from 'express';
 import { ENV } from "./env.js";
 import { SocketAuthMiddleware } from "../middleware/SocketAuthMiddleware.js";
+import Message from "../models/messageModel.js";
+import Group from "../models/groupModel.js";
 
 const app = express()
 const server = http.createServer(app)
@@ -22,14 +24,23 @@ export function getReceiverSocketId(userId) {
     return userSocketMap[userId];
 };
 
+export function getSocketIdByUserId(userId) {
+    return userSocketMap[userId];
+}
+
 // this is for storing online users
 const userSocketMap = {};  //{userId: socketId}
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
     console.log("A user connected " + socket.user.fullName + " with socket ID: " + socket.id);
 
     const userId = socket.userId;
     userSocketMap[userId] = socket.id;
+
+    const userGroups = await Group.find({ members: userId }).select("_id");
+    userGroups.forEach(group => {
+        socket.join(group._id.toString());
+    });
 
     // io.emit() is used to send events to all connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
@@ -113,6 +124,119 @@ io.on("connection", (socket) => {
         const receiverSocketId = userSocketMap[to];
         if (receiverSocketId) {
             io.to(receiverSocketId).emit("typing:stop", { from: userId });
+        }
+    });
+
+    // JOIN GROUP ROOM
+    socket.on("joinGroup", (groupId) => {
+        socket.join(groupId);
+    });
+
+    // LEAVE GROUP ROOM (e.g. when user navigates away)
+    socket.on("leaveGroup", (groupId) => {
+        socket.leave(groupId);
+    });
+
+    // SEND GROUP MESSAGE
+    socket.on("sendGroupMessage", async ({ groupId, text, image }) => {
+        try {
+            const senderId = socket.userId;
+
+            if (!text && !image) {
+                return socket.emit("error", { message: "Message cannot be empty" });
+            }
+
+            const group = await Group.findById(groupId);
+            if (!group || !group.members.some(id => id.toString() === senderId)) {
+                return socket.emit("error", { message: "You are not a member of this group" });
+            }
+
+            const message = await Message.create({ senderId, groupId, text, image });
+
+            const populatedMessage = await message.populate("senderId", "fullName profilePic");
+
+            await Group.findByIdAndUpdate(groupId, {
+                lastMessage: {
+                    text,
+                    image,
+                    sender: senderId,
+                    createdAt: new Date(),
+                },
+            });
+
+            io.to(groupId).emit("newGroupMessage", populatedMessage);
+        } catch (error) {
+            console.log("Error in sendGroupMessage socket:", error.message);
+            socket.emit("error", { message: "Failed to send message" });
+        }
+    });
+
+    // EDIT GROUP MESSAGE
+    socket.on("editGroupMessage", async ({ messageId, groupId, newText }) => {
+        try {
+            const senderId = socket.userId;
+
+            const message = await Message.findById(messageId);
+            if (!message) {
+                return socket.emit("error", { message: "Message not found" });
+            }
+
+            if (message.senderId.toString() !== senderId) {
+                return socket.emit("error", { message: "You can only edit your own messages" });
+            }
+
+            message.text = newText;
+            message.isEdited = true;
+            await message.save();
+
+            // update group's lastMessage only if this was the latest message
+            const group = await Group.findById(groupId);
+            if (group.lastMessage?.createdAt?.getTime() === message.createdAt.getTime()) {
+                group.lastMessage.text = newText;
+                await group.save();
+            }
+
+            io.to(groupId).emit("groupMessageEdited", message);
+        } catch (error) {
+            console.log("Error in editGroupMessage socket:", error.message);
+            socket.emit("error", { message: "Failed to edit message" });
+        }
+    });
+
+    // DELETE GROUP MESSAGE
+    socket.on("deleteGroupMessage", async ({ messageId, groupId }) => {
+        try {
+            const senderId = socket.userId;
+
+            const message = await Message.findById(messageId);
+            if (!message) {
+                return socket.emit("error", { message: "Message not found" });
+            }
+
+            if (message.senderId.toString() !== senderId) {
+                return socket.emit("error", { message: "You can only delete your own messages" });
+            }
+
+            await Message.findByIdAndDelete(messageId);
+
+            // re-sync lastMessage since the deleted one might have been the latest
+            const latestMessage = await Message.findOne({ groupId }).sort({ createdAt: -1 });
+
+            await Group.findByIdAndUpdate(groupId, {
+                lastMessage: latestMessage
+                    ? {
+                        text: latestMessage.text,
+                        image: latestMessage.image,
+                        sender: latestMessage.senderId,
+                        createdAt: latestMessage.createdAt,
+                    }
+                    : null,
+            });
+
+            io.to(groupId).emit("groupMessageDeleted", { messageId, groupId });
+        } catch (error) {
+            console.log("Error in deleteGroupMessage socket:", error.message);
+            socket.emit("error", { message: "Failed to delete message" });
         }
     });
 
